25874f39f5f2c055f8a63c4dcfa003bae5976683
qunwang02 <51785600+qunwang02@users.noreply.github.com>
2026-01-14 20:48:45 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import health from './health.js';
 import submit from './submit.js';
+import getData from './getData.js';
+import deleteData from './deleteData.js';
 
 export default async function handler(req, res) {
   const path = req.url.split('/').pop().split('?')[0];
@@ -8,6 +10,10 @@ export default async function handler(req, res) {
     return health(req, res);
   } else if (path === 'submit') {
     return submit(req, res);
+  } else if (path === 'getData') {
+    return getData(req, res);
+  } else if (path === 'deleteData') {
+    return deleteData(req, res);
   } else {
     res.status(404).json({ error: 'Not Found' });
   }
